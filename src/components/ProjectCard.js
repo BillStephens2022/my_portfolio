f0bad3aef99fc1013306f9b2a8f9a714bf9e0ca8
BillStephens2022 <two4onebill@yahoo.com>
2023-04-07 14:51:30 -0400

    mapped project data and images into project cards and added styling to portfolio page

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+// Project Card component that will render a card displaying a screenshot of the app,
+// a title, a brief description, and buttons that will take the user to the
+// associated Github repo and to the deployed app (only if the app is actually deployed)
+function ProjectCard({ project }) {
+  return (
+    <div className="projects__card">
+      <div className="projects__card--image-container">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/${project.image1}`}
+          className="projects__card--image-photo"
+          alt="project pic"
+        />
+      </div>
+      <div className="projects__card-body">
+        <h5 className="projects__card-body--title">{project.title}</h5>
+        <p className="projects__card-body--text">{project.desc}</p>
+        <div className="projects__card-body--footer">
+        {project.deployedUrl !== "N/A" ? (
+          <a
+            href={project.deployedUrl}
+            className="projects__card-body--footer--link"
+          >
+            <i className="fas fa-link"></i>
+            Deployed App
+          </a>
+        ) : null}
+
+        <a
+          href={project.gitHubRepoUrl}
+          className="projects__card-body--footer--link"
+        >
+          <i className="fab fa-github"></i>
+          Github Repo
+        </a>
+      </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default ProjectCard;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import ProjectCard from '../components/ProjectCard';
+import { projects } from '../data/projects';
+import "../css/style.css";
+
+// Portfolio Page displaying ProjectCard components
+
 function Portfolio() {
-    return (
-        <div>
-            <h1>My Portfolio</h1>
-        </div>
-    )
+  return (
+    <div className="portfolio">
+      <div className="portfolio__hero">
+        <h1 className="portfolio__hero-header">My Project Portfolio</h1>
+        <img className="portfolio__hero-logo" src="../../images/webdev.png" alt="web dev logo"/>
+      </div>
+      
+      <div className="projects">
+        {projects.map((project) => (
+          <ProjectCard project={project} key={project.id}/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
