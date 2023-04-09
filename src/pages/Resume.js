b9768713532f08dd1cb42d3c795116c07197abe1
BillStephens2022 <two4onebill@yahoo.com>
2023-04-09 10:25:23 -0400

    fixed conflicting css between resume page and other pages

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -26,19 +26,19 @@ function Resume() {
         Download PDF
       </button>
       <div className="body-div resume-div">
-        <div class="row">
-          <div class="col-sm-4 col-md-3 col-lg-2 photo">
+        <div className="row">
+          <div className="col-sm-4 col-md-3 col-lg-2 photo">
             <img
-              class="align-items-center bill-pic"
+              className="align-items-center bill-pic"
               src={`${process.env.PUBLIC_URL}/images/bill.jpeg`}
               alt="Bill pic"
             />
           </div>
-          <div class="col-sm-8 col-md-9 col-lg-10 summary">
-            <h1 class="header-name">Bill Stephens</h1>
-            <h3 class="header-occupation">Full Stack Web Developer</h3>
-            <hr class="header-hr" />
-            <h3 class="header-summary">
+          <div className="col-sm-8 col-md-9 col-lg-10 summary">
+            <h1 className="header-name">Bill Stephens</h1>
+            <h3 className="header-occupation">Full Stack Web Developer</h3>
+            <hr className="header-hr" />
+            <h3 className="header-summary">
               Full Stack Web Developer with an extensive background in
               Accounting and Finance in the Investment Banking industry. Known
               for ability to distill complex problems into easy to understand
@@ -50,46 +50,46 @@ function Resume() {
             </h3>
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-4 col-md-3 col-lg-2 contact-info-technical">
-            <p class="icon contact-info">
-              <i class="fa fa-envelope" aria-hidden="true"></i>
+        <div className="row">
+          <div className="col-sm-4 col-md-3 col-lg-2 contact-info-technical">
+            <p className="icon contact-info">
+              <i className="fa fa-envelope" aria-hidden="true"></i>
             </p>
-            <p class="email contact-info contact-details">
+            <p className="email contact-info contact-details">
               stephensbill17@gmail.com
             </p>
-            <p class="icon contact-info">
-              <i class="fa fa-mobile" aria-hidden="true"></i>
+            <p className="icon contact-info">
+              <i className="fa fa-mobile" aria-hidden="true"></i>
             </p>
-            <p class="phone contact-info contact-details">(201)-850-7410</p>
-            <p class="icon contact-info">
-              <i class="fa fa-solid fa-location-dot"></i>
+            <p className="phone contact-info contact-details">(201)-850-7410</p>
+            <p className="icon contact-info">
+              <i className="fa fa-solid fa-location-dot"></i>
             </p>
-            <p class="location contact-info contact-details">Toms River, NJ</p>
-            <p class="icon contact-info">
-              <i class="fa fa-brands fa-linkedin"></i>
+            <p className="location contact-info contact-details">Toms River, NJ</p>
+            <p className="icon contact-info">
+              <i className="fa fa-brands fa-linkedin"></i>
             </p>
-            <p class="linkedin contact-info contact-details">
+            <p className="linkedin contact-info contact-details">
               <a href="https://www.linkedin.com/in/bill-stephens-04375923/">
                 LinkedIn Profile
               </a>
             </p>
-            <p class="icon contact-info">
-              <i class="fa fa-solid fa-diagram-project"></i>
+            <p className="icon contact-info">
+              <i className="fa fa-solid fa-diagram-project"></i>
             </p>
-            <p class="portfolio contact-info contact-details">
+            <p className="project-portfolio contact-info contact-details">
               <a href="https://billstephens2022.github.io/ProjectPortfolio/">
                 Portfolio/Projects
               </a>
             </p>
-            <p class="icon contact-info">
-              <i class="fa fa-brands fa-github"></i>
+            <p className="icon contact-info">
+              <i className="fa fa-brands fa-github"></i>
             </p>
-            <p class="github contact-info contact-details">
+            <p className="github contact-info contact-details">
               <a href="https://github.com/BillStephens2022"> Github</a>
             </p>
-            <div class="technical-skills">
-              <p class="section-headers">TECHNICAL SKILLS</p>
+            <div className="technical-skills">
+              <p className="section-headers">TECHNICAL SKILLS</p>
               <hr />
               <ul>
                 <li>HTML</li>
@@ -106,11 +106,11 @@ function Resume() {
               </ul>
             </div>
           </div>
-          <div class="col-sm-8 col-md-9 col-lg-10 section-headers projects">
-            <h2 class="section-headers">
+          <div className="col-sm-8 col-md-9 col-lg-10 section-headers section-projects">
+            <h2 className="section-headers">
               PROJECTS (
               <a
-                class="portfolio-link"
+                className="project-portfolio-link"
                 href="https://billstephens2022.github.io/ProjectPortfolio/"
               >
                 Portfolio page
@@ -118,15 +118,15 @@ function Resume() {
               )
             </h2>
             <hr />
-            <div class="project-1">
+            <div className="project-1">
               <h3>
                 Expense Tracker
-                <span class="links link-left">
+                <span className="links link-left">
                   <a href="https://expense-tracker-mern.herokuapp.com">
                     Deployed App
                   </a>
                 </span>
-                <span class="links">
+                <span className="links">
                   {" "}
                   |{" "}
                   <a href="https://github.com/BillStephens2022/Expense-Tracker-MERN">
@@ -138,7 +138,7 @@ function Resume() {
                 Full Stack MERN app which allows users to track personal
                 expenses by category (Housing, Food, Entertainment, etc.).
               </h6>
-              <ul class="project-list">
+              <ul className="project-list">
                 <li>
                   Team collaboration - My contributions were across both front
                   end (React, Chart.js) and back end (MongoDB, Apollo
@@ -150,15 +150,15 @@ function Resume() {
                 </li>
               </ul>
             </div>
-            <div class="project-2">
+            <div className="project-2">
               <h3>
                 Community Chat
-                <span class="links link-left">
+                <span className="links link-left">
                   <a href="https://community-chat.herokuapp.com">
                     Deployed App
                   </a>
                 </span>
-                <span class="links">
+                <span className="links">
                   {" "}
                   |{" "}
                   <a href="https://github.com/BillStephens2022/Community-Chat">
@@ -171,7 +171,7 @@ function Resume() {
                 profile, create posts, attach photos/videos, and comment on
                 other users’ posts.
               </h6>
-              <ul class="project-list">
+              <ul className="project-list">
                 <li>
                   Team collaboration - My contributions were across both front
                   end (HTML, CSS, Handlebars) and back end (mySQL, Sequelize,
@@ -183,15 +183,15 @@ function Resume() {
                 </li>
               </ul>
             </div>
-            <div class="project-3">
+            <div className="project-3">
               <h3>
                 Vacation Planner
-                <span class="links link-left">
+                <span className="links link-left">
                   <a href="https://jsedlak146.github.io/group8-vacationplanner">
                     Deployed App
                   </a>
                 </span>
-                <span class="links">
+                <span className="links">
                   {" "}
                   |{" "}
                   <a href="https://github.com/jsedlak146/group8-vacationplanner">
@@ -203,7 +203,7 @@ function Resume() {
                 Vacation Planner allows users to search a city and date and find
                 available AirBnB rentals, local events, and local breweries.
               </h6>
-              <ul class="project-list">
+              <ul className="project-list">
                 <li>
                   Team collaboration - My contributions include working with the
                   Ticketmaster and Open Brewery APIs, and to render cards on the
@@ -217,29 +217,29 @@ function Resume() {
                 </li>
               </ul>
             </div>
-            <div class="col-sm-8 col-md-9 col-lg-10 section-headers work-experience">
-              <h2 class="section-headers">WORK EXPERIENCE</h2>
+            <div className="col-sm-8 col-md-9 col-lg-10 section-headers work-experience">
+              <h2 className="section-headers">WORK EXPERIENCE</h2>
               <hr />
-              <div class="MorganStanley">
-                <div class="job-header">
+              <div className="MorganStanley">
+                <div className="job-header">
                   <img
-                    class="MSLogo"
+                    className="MSLogo"
                     src={`${process.env.PUBLIC_URL}/images/MSLogo.png`}
                     alt="MS Logo"
                   />
-                  <span class="job-location">New York, NY</span>
-                  <span class="job-dates">May 2014 - May 2022</span>
+                  <span className="job-location">New York, NY</span>
+                  <span className="job-dates">May 2014 - May 2022</span>
                 </div>
-                <p class="role">
+                <p className="role">
                   Vice President - Relationship Lending Controller
                 </p>
-                <h6 class="job-summary">
+                <h6 className="job-summary">
                   Managed a team of 7 across New York and Baltimore responsible
                   for maintaining the general ledger and financial reporting for
                   a $90 billion corporate loan portfolio.
                 </h6>
-                <p class="accomplishments">Key Accomplishments:</p>
-                <ul class="work-bullets">
+                <p className="accomplishments">Key Accomplishments:</p>
+                <ul className="work-bullets">
                   <li>
                     Collaborated with Technology and several cross functional
                     areas to analyze and implement new accounting standards –
@@ -261,39 +261,39 @@ function Resume() {
                 </ul>
               </div>
               <hr />
-              <div class="CreditSuisse">
-                <div class="job-header">
+              <div className="CreditSuisse">
+                <div className="job-header">
                   <img
-                    class="CSLogo"
+                    className="CSLogo"
                     src={`${process.env.PUBLIC_URL}/images/CSLogo.png`}
                     alt="Credit Suisse Logo"
                   />
-                  <span class="job-location">New York, NY</span>
-                  <span class="job-dates">Oct 1996 - Sep 2013</span>
+                  <span className="job-location">New York, NY</span>
+                  <span className="job-dates">Oct 1996 - Sep 2013</span>
                 </div>
-                <p class="role">
+                <p className="role">
                   Director - Valuation Risk Group - Fair Value Control Team (Jan
                   2013 - Sep 2013)
                 </p>
-                <h6 class="job-summary">
+                <h6 className="job-summary">
                   Managed a team of 3 responsible for centrally controlling the
                   Fair Value Disclosure processes across all businesses within
                   the investment bank.
                 </h6>
               </div>
-              <div class="CreditSuisse">
-                <p class="role">
+              <div className="CreditSuisse">
+                <p className="role">
                   Director - Americas Head of Investment Banking Product Control
                   (Jan 2008 - Dec 2012)
                 </p>
-                <h6 class="job-summary">
+                <h6 className="job-summary">
                   Managed a team of 15 across New York and Raleigh responsible
                   for maintaining the general ledger and financial reporting for
                   Corporate Lending, Loan Capital Markets, Private Banking
                   Lending, Equity Capital Markets, and Mergers & Acquisitions.
                 </h6>
-                <p class="accomplishments">Key Accomplishments:</p>
-                <ul class="work-bullets">
+                <p className="accomplishments">Key Accomplishments:</p>
+                <ul className="work-bullets">
                   <li>
                     Earned a “Credit Suisse Award” for leading the Fair Value
                     Option project for Corporate Loans which included
@@ -311,39 +311,39 @@ function Resume() {
                     of a new loan booking system (Loan IQ).
                   </li>
                 </ul>
-                <h6 class="job-note">
+                <h6 className="job-note">
                   Note: Various other roles (promoted 4 times) held between
                   1996-2007
                 </h6>
               </div>
-              <div class="education">
-                <h2 class="section-headers">EDUCATION</h2>
+              <div className="education">
+                <h2 className="section-headers">EDUCATION</h2>
                 <hr />
 
-                <div class="row">
-                  <div class="col-sm-4 col-md-4 col-lg-5 degrees">
+                <div className="row">
+                  <div className="col-sm-4 col-md-4 col-lg-5 degrees">
                     <h3>Graduate and Undergraduate Degrees</h3>
                     <br />
-                    <h6 class="degree">MBA, Professional Accounting</h6>
-                    <h6 class="college">Rutgers University</h6>
-                    <h6 class="time-range">1995-1996</h6>
+                    <h6 className="degree">MBA, Professional Accounting</h6>
+                    <h6 className="college">Rutgers University</h6>
+                    <h6 className="time-range">1995-1996</h6>
                     <br />
-                    <h6 class="degree">B.S. Administration of Justice</h6>
-                    <h6 class="college">Rutgers University</h6>
-                    <h6 class="time-range">1990 - 1994</h6>
+                    <h6 className="degree">B.S. Administration of Justice</h6>
+                    <h6 className="college">Rutgers University</h6>
+                    <h6 className="time-range">1990 - 1994</h6>
                   </div>
-                  <div class="col-sm-4 col-md-4 col-lg-5 degrees">
+                  <div className="col-sm-4 col-md-4 col-lg-5 degrees">
                     <h3>Coding Bootcamps</h3>
                     <br />
-                    <h6 class="degree">Rutgers Coding Bootcamp</h6>
-                    <h6 class="college">Rutgers University</h6>
-                    <h6 class="time-range">2022 - present</h6>
+                    <h6 className="degree">Rutgers Coding Bootcamp</h6>
+                    <h6 className="college">Rutgers University</h6>
+                    <h6 className="time-range">2022 - present</h6>
                     <br />
-                    <h6 class="degree text-wrap">
+                    <h6 className="degree text-wrap">
                       The Complete 2022 Web Development Bootcamp
                     </h6>
-                    <h6 class="college">Udemy, Instructor: Dr Angela Yu</h6>
-                    <h6 class="time-range">2022</h6>
+                    <h6 className="college">Udemy, Instructor: Dr Angela Yu</h6>
+                    <h6 className="time-range">2022</h6>
                   </div>
                 </div>
               </div>
