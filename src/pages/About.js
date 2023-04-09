8c423d978ab5514edf45330baef7889f59c9287a
BillStephens2022 <two4onebill@yahoo.com>
2023-04-09 11:16:10 -0400

    updated styling on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,34 +4,34 @@ function About() {
   return (
     <div className="about">
       <div className="about__video">
-          <video className="about__video--content" autoPlay muted loop>
-            <source src="../../images/video3.mp4" type="video/mp4"/>
-            <source src="../../images/video.webm" type="video/webm"/>
-            Your browser is not supported!
-          </video>
+        <video className="about__video--content" autoPlay muted loop>
+          <source src="../../images/video3.mp4" type="video/mp4" />
+          <source src="../../images/video.webm" type="video/webm" />
+          Your browser is not supported!
+        </video>
       </div>
       <div className="about__hero">
-        <img
-          className="about__photo"
-          src="../../images/bill.jpeg"
-          alt="Bill"
-        />
+        <img className="about__photo" src="../../images/bill.jpeg" alt="Bill" />
         <div className="about__headers">
           <h1 className="about__title">Bill Stephens</h1>
           <h2 className="about__subtitle">Full Stack Web Developer</h2>
         </div>
       </div>
-      <p className="about__story">
+      <h6 className="about__story">
         Full Stack Web Developer with a 25 year background in Finance/Accounting
         in the Investment Banking industry. I am looking to leverage by finance
         background with my newly acquired programming skills. Let's build
         something together!
-      </p>
+      </h6>
       <div className="skills">
         <h2 className="skills__header">Skills</h2>
         <div className="skills__container">
           <div className="skills__card card-1">
-            <h4 className="skills__card--header"><img id="react" src="../../images/react.png" alt="react logo" />Front End<img id="css3" src="../../images/css3.png" alt="css3 logo" /></h4>
+            <h4 className="skills__card--header">
+              <img id="react" src="../../images/react.png" alt="react logo" />
+              Front End
+              <img id="css3" src="../../images/css3.png" alt="css3 logo" />
+            </h4>
             <ul className="skills__card--list">
               <li>JavaScript</li>
               <li>React.js</li>
@@ -46,8 +46,13 @@ function About() {
               <li>Tailwind</li>
             </ul>
           </div>
+
           <div className="skills__card card-2">
-            <h4 className="skills__card--header"><img id="node" src="../../images/node.png" alt="node logo" />Back End<img id="mongo" src="../../images/mongo.png" alt="mongo logo" /></h4>
+            <h4 className="skills__card--header">
+              <img id="node" src="../../images/node.png" alt="node logo" />
+              Back End
+              <img id="mongo" src="../../images/mongo.png" alt="mongo logo" />
+            </h4>
             <ul className="skills__card--list">
               <li>Node.js</li>
               <li>Express.js</li>
@@ -58,19 +63,25 @@ function About() {
               <li>Apollo Server</li>
               <li>Passport.js</li>
               <li>JSON Web Token</li>
-
-              
             </ul>
           </div>
+
           <div className="skills__card card-3">
-            <h4 className="skills__card--header">Python<img id="python" src="../../images/python.png" alt="python logo" /></h4>
+            <h4 className="skills__card--header">
+              Python
+              <img
+                id="python"
+                src="../../images/python.png"
+                alt="python logo"
+              />
+            </h4>
             <ul className="skills__card--list">
-                <li>Flask</li>
-                <li>Pandas</li>
-                <li>Numpy</li>
-                <li>Tkinter</li>
-                <li>Beautiful Soup</li>
-                <li>3rd Party APIs</li>
+              <li>Flask</li>
+              <li>Pandas</li>
+              <li>Numpy</li>
+              <li>Tkinter</li>
+              <li>Beautiful Soup</li>
+              <li>3rd Party APIs</li>
             </ul>
           </div>
         </div>
