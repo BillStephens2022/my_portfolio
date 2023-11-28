339f8ea050a6ceb07df29a1e89ed8719eadd70df
BillStephens2022 <two4onebill@yahoo.com>
2023-11-28 13:51:41 -0500

    adjusted styling of photo nav buttons on project cards & adjusted responsiveness of modals

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -27,19 +27,25 @@ function ProjectCard({ project, onImageClick }) {
           className="projects__card--image-photo"
           alt="project pic"
           onClick={() => onImageClick(`${process.env.PUBLIC_URL}/images/${project.images[currentImageIndex]}`)}
-        />      
-          {project.images.length > 1 && (
+        />
+        {project.images.length > 1 && (
           <div className="projects__card--image-buttons">
-            <button className="projects__card--image-container-forward" onClick={handlePrevImage}><GoChevronLeft /></button>
-            <p className="projects__card--image-container-p">see more photos</p>
-            <button className="projects__card--image-container-back" onClick={handleNextImage}><GoChevronRight /></button>
+            <div className="projects__card--image-buttons_div" onClick={handlePrevImage}>
+              <button className="projects__card--image-container-forward"><GoChevronLeft /></button>
+              <p className="projects__card--image-container-p">prev</p>
+            </div>
+            <p className="projects__card--image-container-p2">photos</p>
+            <div className="projects__card--image-buttons_div" onClick={handleNextImage}>
+              <p className="projects__card--image-container-p">next</p>
+              <button className="projects__card--image-container-back" onClick={handleNextImage}><GoChevronRight /></button>
+            </div>
           </div>
         )}
       </div>
       <div className="projects__card-body">
         <h5 className="projects__card-body--title">{project.title}</h5>
         <p className="projects__card-body--text">{project.desc}</p>
-        
+
       </div>
       <div className="projects__card-footer">
         {project.deployedUrl !== "N/A" ? (
