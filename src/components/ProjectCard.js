650a39d4e53ab758df72dffbee5616dc5e76a87f
BillStephens2022 <two4onebill@yahoo.com>
2023-11-28 11:09:12 -0500

    added chevron buttons to enable scrolling of photos

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,38 @@
+import { useState } from "react";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+
 // Project Card component that will render a card displaying a screenshot of the app,
 // a title, a brief description, and buttons that will take the user to the
 // associated Github repo and to the deployed app (only if the app is actually deployed)
 function ProjectCard({ project }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === project.images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? project.images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="projects__card">
       <div className="projects__card--image-container">
         <img
-          src={`${process.env.PUBLIC_URL}/images/${project.image1}`}
+          src={`${process.env.PUBLIC_URL}/images/${project.images[currentImageIndex]}`}
           className="projects__card--image-photo"
           alt="project pic"
         />
+          {project.images.length > 1 && (
+          <div className="projects__card--image-buttons">
+            <button className="projects__card--image-container-forward" onClick={handlePrevImage}><GoChevronLeft /></button>
+            <button className="projects__card--image-container-back" onClick={handleNextImage}><GoChevronRight /></button>
+          </div>
+        )}
       </div>
       <div className="projects__card-body">
         <h5 className="projects__card-body--title">{project.title}</h5>
