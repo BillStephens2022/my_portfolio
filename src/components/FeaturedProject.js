5b712b242e4d580742f926957077574e00cd1127
BillStephens2022 <two4onebill@yahoo.com>
2023-12-18 17:18:31 -0500

    filtered featured projects from projects data and rendered in FeaturedProject component and Featured page

--- src/components/FeaturedProject.js
+++ src/components/FeaturedProject.js
@@ -3,7 +3,9 @@
 function FeaturedProject({ featuredProject }) {
   return (
     <div className="featuredProject">
-        <h2>{featuredProject.title}</h2>
+        <h2 className="featuredProject__header">{featuredProject.title}</h2>
+        <img className="featuredProject__image" src={`${process.env.PUBLIC_URL}/images/${featuredProject.images[0]}`}></img>
+        <p className="featuredProject__desc">{featuredProject.desc}</p>
     </div>
   )
 }
