9f15950c6d6e4b159be877345cdefd43c5475b89
BillStephens2022 <two4onebill@yahoo.com>
2024-06-23 06:42:44 -0400

    updated footer to use a dynamic copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
     return (
         <footer className="footer">
-            <h1 className="footer__contact">&copy; 2024 by Bill Stephens, all rights reserved.</h1>
+            <h1 className="footer__contact">&copy; {currentYear} by Bill Stephens, all rights reserved.</h1>
             <a href="https://github.com/BillStephens2022" className="footer__icon"><i className="fab fa-github"></i></a>
             <a href="https://www.linkedin.com/in/bill-stephens-04375923/" className="footer__icon"><i className="fab fa-linkedin"></i></a>
             <a href="https://instagram.com/two4onebill?igshid=YmMyMTA2M2Y" className="footer__icon"><i className="fab fa-instagram"></i></a>
